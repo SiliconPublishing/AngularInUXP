5cddf75d7a822d4dcc58f25ce891fdc6038cbd29
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2025-05-28 15:43:41 -0700

    Updated documentation

--- projects/example-application/src/app/app.module.ts
+++ projects/example-application/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { ExampleLibraryModule } from '@library';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
     providers: [provideZoneChangeDetection({ eventCoalescing: true })],
     declarations: [
         AppComponent,
     ],
-    imports: [BrowserModule, FontAwesomeModule, ExampleLibraryModule],
+    imports: [BrowserModule, ExampleLibraryModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
